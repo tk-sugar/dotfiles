6f2bbd68bc75f12a91cad31cba324a114b9545b7
tk-sugar <takuma.sato@jmty.jp>
2019-11-13 13:20:13 +0900

    add gemrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_profile .bash_aliases .vimrc .gitconfig .git-completion.bash .git-prompt.sh .tmux.conf .vim/rc .zshrc .gitignore_global)
+DOT_FILES=(.bashrc .bash_profile .bash_aliases .vimrc .gitconfig .git-completion.bash .git-prompt.sh .tmux.conf .vim/rc .zshrc .gitignore_global .gemrc)
 
 for file in ${DOT_FILES[@]}
 do
